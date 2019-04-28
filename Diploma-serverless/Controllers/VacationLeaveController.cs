adcaf9cb32f6e8ff4b6de4ddb5178beee9aae9f2
Blaž <blaz.rupnik56@gmail.com>
2019-04-28 19:06:21 +0200

    starting code, ef setup, some basic controllers

---
+++ Diploma-serverless/Controllers/VacationLeaveController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DbDataAccess;
+
+namespace Diploma_serverless.Controllers
+{
+    public class VacationLeaveController : ApiController
+    {
+        public IEnumerable<APP_VacationLeave> Get()
+        {
+            using(var context = new DiplomaDbEntities())
+            {
+                return context.APP_VacationLeave.ToList();
+            }
+        }
+
+        public APP_VacationLeave Post(APP_VacationLeave model)
+        {
+            using(var context = new DiplomaDbEntities())
+            {
+                model.ID_VacationLeave = Guid.NewGuid();
+                context.APP_VacationLeave.Add(model);
+                context.SaveChanges();
+                return model;
+            }
+        }
+    }
+}
